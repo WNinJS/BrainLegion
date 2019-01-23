f2413f60a77ded6c5272f3cf5d9252fa4517d8a9
Max <beardworf@gmail.com>
2019-01-23 18:48:19 +0500

    Create adminlogin.php

---
+++ brainlegion.ru/public_html/adminlogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+ 	<head>
+	    <meta charset="utf-8">
+	    <title>BrainLegion Studio</title>
+
+	    <!-- TODO: SEO -->
+	    <!-- TODO: Теги у картинок -->
+	    <!-- TODO: Модальные окна -->
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-grid.min.css">
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-reboot.css">
+
+	    <!-- Fontello Fonts -->
+	    <link rel="stylesheet" href="res/fontello/css/fontello.css">
+
+	    <!-- Own Styles -->
+	    <link rel="stylesheet" href="cssadmin/adminlogin.css">
+	</head>
+<body>
+	<article class="container d-flex justify-content-center h-100 w-100">
+		<div class="row d-flex justify-content-center w-100 h-50">
+			<div class="col-sm-5" style="background-color: black;">
+			</div>
+		</div>
+	</article>
+</body>
+</html>
