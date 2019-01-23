32e8e6266aa53fb5fc467ea9dd3af9fc4c371ce1
Zudwa-8 <34678214+Zudwa-8@users.noreply.github.com>
2019-01-23 23:34:13 +0500

    Create adminpanel.php

---
+++ brainlegion.ru/public_html/adminpanel.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+	    <title>BrainLegion Admin</title>
+
+	    <!-- TODO: SEO -->
+	    <!-- TODO: Теги у картинок -->
+	    <!-- TODO: Модальные окна -->
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-grid.min.css">
+	    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-reboot.css">
+
+	    <!-- Fontello Fonts -->
+	    <link rel="stylesheet" href="res/fontello/css/fontello.css">
+
+        <!-- Own Styles -->
+        <link rel="stylesheet" href="res/styles.css">
+	    <link rel="stylesheet" href="cssadmin/adminpanel.css">
+	</head>
+<body>
+    <!-- Top Navigation -->
+    <div class="container-fluid px-0">
+        <nav class="navbar fixed-top-desktop px-md-5 navbar-expand-lg navbar-light" id="top-menu">
+            <!-- Logo -->
+            <div id="dropdown-menu" class="dropdown-menu collapse navbar-collapse">
+                <span class="title">
+                <img src="res/img/logos/brainlegion_main.svg" alt="FILL" class="img-fluid img-logo">
+                </span>
+                <ul class="dropdown-menu navbar-nav ">
+                    <li><a href="#" class="nav-link text-purple">Информация</a></li>
+                    <li><a href="#" class="nav-link text-purple">Главная</a></li>
+                    <li><a href="#" class="nav-link text-purple">Studio</a></li>
+                    <li><a href="#" class="nav-link text-purple">Factory</a></li>
+                    <li><a href="#" class="nav-link text-purple">Logout</a></li>
+                </ul>
+            </div>
+            <!-- Menu Button for Mobile -->
+            <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Menu Itself -->
+          
+        </nav>
+    </div>
+    <!-- Top Navigation -->
+    <script src="res/js/dropMenu.js"></script>
+</body>
+</html>
