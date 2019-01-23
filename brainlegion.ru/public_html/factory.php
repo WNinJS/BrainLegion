8e5a2c1ab65dccdd61b09402eccc02f4a69a585c
Max <beardworf@gmail.com>
2019-01-23 17:04:26 +0500

    Create factory.php

---
+++ brainlegion.ru/public_html/factory.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>BrainLegion Factory</title>
+
+    <!-- TODO: SEO -->
+    <!-- TODO: Теги у картинок -->
+    <!-- TODO: Модальные окна -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="res/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="res/bootstrap/css/bootstrap-reboot.css">
+
+    <!-- Fontello Fonts -->
+    <link rel="stylesheet" href="res/fontello/css/fontello.css">
+
+    <!-- Own Styles -->
+    <link rel="stylesheet" href="res/styles.css">
+  </head>
+  <body>
+
+    <!-- Top Menu Section -->
+    <section class="container-fluid sticky-top-desktop px-0" id="top-menu">
+
+      <!-- Top Navigation -->
+      <div class="container-fluid px-0">
+        <nav class="navbar px-md-5 navbar-expand-lg navbar-light">
+
+          <!-- Logo -->
+          <a href="index.php" class="navbar-brand ">
+            <img src="res/img/logos/brainlegion_factory.svg" alt="FILL" class="img-fluid img-logo">
+          </a>
+
+          <!-- Menu Button for Mobile -->
+          <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon "></span>
+          </button>
+
+          <!-- Menu Itself -->
+          <div class="collapse navbar-collapse text-right" id="navbar-toggler">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link text-purple" href="index.php"><span>На главную</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-purple" href="#competences"><span>Услуги</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-purple" href="#contact"><span>Связаться</span></a>
+              </li>
+            </ul>
+          </div>
+
+        </nav>
+      </div><!-- Top Navigation -->
+
+    </section><!-- Top Menu Section -->
+
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid jumbotron-light m-0 p-5 text-light" id="jumbotron-about">
+      <div class="container">
+        <h1 class="text-purple">BrainLegion Factory</h1>
+        <p class="lead">Анализируем проблему, включаем машину творческого потенциала, собираем  и генерируем лучшие гипотезы и практики, создаем решения, тестируем их и внедряем!</p>
+      </div>
+    </div>
+
+
+    <!-- About Section -->
+    <section class="container-fluid py-5" id="about">
+
+      <!-- Snippet -->
+      <div class="container py-2">
+
+        <p class="mt-4">Мы предоставляем услуги по выполнению научно-исследовательских работ (НИОКР) для частных и юридических лиц.</p>
+        <p class="mt-4">Мы выполняем фундаментальные (теоретические) исследования, разрабатываем стратегии и программы развития территорий и муниципальных образований, концепции и обоснования, маркетинговые и социологические исследования, программы модернизации образования, прикладные исследования по заказу органов исполнительной власти, положения, стандарты, научно-исследовательские работы по бюджетно-налоговой тематике, проблемам развития экономики регионов и муниципальных образований, концепции повышения имиджа территорий  и мн. др.</p>
+      </div>
+
+    </section>
+
+
+    <!-- Competences Section -->
+    <section class="container-fluid py-5" id="competences">
+      <h1 class="text-center text-purple">Услуги</h1>
+
+      <!-- Competency -->
+      <div class="container mt-4 py-2">
+        <h2 class="text-center">Мероприятия под ключ</h2>
+
+        <!-- Competency Row -->
+        <div class="row mx-auto mt-4 py-2 justify-content-center">
+
+          <div class="card-container col-sm-12 col-md-6 col-lg-8 p-0">
+            <div class="card card-half-transparent h-100">
+              <div class="card-body d-flex flex-column">
+                <h4 class="card-title text-center"><b>НИОКРТ «под ключ»</b></h4>
+                <ul>  
+                    <li>Проведение исследований, разработка технического предложения (аванпроекта)</li>
+                        <hr style="width: 100%"  class="mt-0"> 
+                    <li>Разработка эскизного проекта; разработка технического проекта</li>
+                        <hr style="width: 100%"  class="mt-0">
+                    <li>Разработка технического задания на опытно-конструкторские (технологические) работы</li>
+                        <hr style="width: 100%"  class="mt-0">
+                    <li>Разработка эскизного проекта; разработка технического проекта</li>
+                        <hr style="width: 100%"  class="mt-0">
+                    <li>Разработка рабочей конструкторской документации на изготовление опытного образца</li>
+                        <hr style="width: 100%"  class="mt-0">
+                    <li>Отработка документации</li>
+                        <hr style="width: 100%" class="mt-0"> 
+                    <li>Утверждение рабочей конструкторской документации для организации промышленного (серийного) производства изделий</li>
+                        <hr style="width: 100%" class="mt-0"> 
+                    <li>Корректировка конструкторской документации по выявленным скрытым недостаткам</li>
+                        <hr style="width: 100%" class="mt-0">
+                    <li>Разработка эксплуатационной документации</li>
+                        <hr style="width: 100%" class="mt-0">
+                    <li>Разработка рабочей конструкторской документации на проведение ремонтных работ</li>
+                        <hr style="width: 100%" class="mt-0">
+                </ul> 
+                <a href="#" class="mt-auto text-center"><p class="btn btn-purple-outline" data-ordertype="research">Заказать</p></a>
+              </div>
+            </div>
+          </div>
+
+        </div><!--Competency Row -->
+      </div><!-- Competency -->
+
+    </section><!-- Competences Section -->
+
+
+    <!-- Contact Section -->
+    <section class="container-fluid py-5" id="contact">
+
+      <h1 class="text-center text-purple">Сделать заказ</h1>
+
+      <!-- Contact Form -->
+      <!-- ГАЗИНУР, тут нужно сделать, чтобы эти данные добавлялись в базу -->
+      <form method="post" enctype="multipart/form-data" class="d-flex flex-column container pt-4 justify-center align-items-center" id="contact-form">
+
+        <!-- Name & Subject -->
+        <div class="row mx-auto text-center d-flex flex-column align-items-center">
+          <div class="form-group col-md-6">
+            <label for="name">Ваше имя:</label>
+            <input class="form-control form-purple" id="name" type="text" placeholder="Иван Иванов" name="name">
+            <p id="name-error" class="hidden invalid-feedback small">Напишите своё имя</p>
+        	</div>
+
+          <div class="form-group col-md-6">
+            <label for="subject">Тема обращения:</label>
+      			<select class="form-control form-purple" id="subjectSelect">
+      				<option value="research">НИОКРТ «под ключ»</option>
+      			 	<option value="other">Другое</option>
+      			</select>
+        	</div>
+        </div>
+
+        <!-- Phone & Email -->
+        <div class="row mx-auto text-center d-flex flex-column align-items-center">
+          <div class="form-group col-md-6">
+            <label for="phone">Телефон:</label>
+            <input class="form-control form-purple" id="phone" type="text" placeholder="8 123 1231212" name="phone" onclick="addPhone(this)" oninput="validatePhone(this)" onchange="checkPhone(this)">
+            <p id="phone-error" class="hidden invalid-feedback small">Введите корректный номер телефона</p>
+          </div>
+
+
+          <div class="form-group col-md-6">
+            <label for="email">Email:</label>
+            <input class="form-control form-purple" type="text" id="email" placeholder="ivanivanov@domain.com" name="email" onchange="checkMail(this)">
+            <p id="email-error" class="hidden invalid-feedback small">Введите корректный email</p>
+          </div>
+        </div>
+
+        <!-- Message -->
+        <div class="row mx-auto text-center d-flex flex-column align-items-center">
+          <div class="form-group col-sm-6">
+            <label for="text">Сообщение:</label>
+    				<textarea class="form-control form-purple" id="text" name="text" rows="5"></textarea>
+    				<p id="textarea-error" class="hidden invalid-feedback small">Сообщение не может быть пустым</p>
+    			</div>
+        </div>
+
+        <!-- File -->
+        <div class="row mx-auto text-center d-flex flex-column align-items-center">
+          <div class="form-group col-sm-6">
+            <label for="file">Прикрепите файл при необходимости:</label>
+            <label class="custom-file w-100">
+              <input type="file" name="file" id="file" class="form-control form-purple custom-file-input">
+              <span class="form-control form-purple custom-file-control" id="upload-path">Выберите файл...</span>
+            </label>
+          </div>
+        </div>
+
+        <div class="text-center mt-3">
+          <input type="submit" class="btn pointer btn-purple-outline" value="Отправить сообщение">
+          <p id="contact-form-status" class="hidden mt-2">Статус сообщения</p>
+        </div>
+
+      </form>
+
+    </section><!-- Contact Section -->
+
+
+    <!-- Footer -->
+    <section class="container-fluid" id="footer">
+      <div class="container">
+
+        <div class="row justify-content-center text-light py-5">
+
+          <!-- Left Side -->
+          <div class="col-sm-12 col-md-6 col-lg-6" id="footer-contacts">
+
+            <!-- Footer Menu -->
+            <ul class="nav mb-4" id="footer-menu">
+              <li class="nav-item pr-3">
+                <a class="nav-link p-0" href="index.php">Главная</a>
+              </li>
+              <li class="nav-item pr-3">
+                <a class="nav-link p-0" href="studio.php">Studio</a>
+              </li>
+              <li class="nav-item pr-3">
+                <a class="nav-link p-0" href="scholae.php">Scholae</a>
+              </li>
+              <li class="nav-item pr-3">
+                <a class="nav-link p-0" href="factory.php">Factory</a>
+              </li>
+            </ul>
+
+            <!-- Title & Snippet -->
+            <div id="snippet" class="mb-4">
+              <h5>BrainLegion</h5>
+              <p class="text-muted">Оживляем идеи для решения Ваших задач</p>
+            </div>
+
+            <!-- Contacts (phone, email, address, etc) -->
+            <div id="contacts" class="mb-4">
+              <ul class="list-unstyled">
+                <li>+7 (937) 326-11-85</li>
+                <li>+7 (961) 364-08-86</li>
+                <li><a href="mailto:brainlegionufa@gmail.com">brainlegionufa@gmail.com</a></li>
+                <li>Адрес: г. Уфа, Проспект Октября, 2</li>
+              </ul>
+            </div>
+
+            <!-- Social Links -->
+            <!--
+            <div id="socials" class="mb-4">
+              <a href="#"><i class="icon icon-purple icon-vkontakte icon-standart"></i></a>
+              <a href="#"><i class="icon icon-purple icon-instagram icon-standart"></i></a>
+              <a href="#"><i class="icon icon-purple icon-facebook-squared icon-standart"></i></a>
+            </div>
+            -->
+
+            <!-- Copyrights -->
+            <div id="copyrights">
+              <p>Все права защищены </p>
+              <p>© <a class="link-footer" href="#">Brainlegion.ru</a>, 2018</p>
+            </div>
+
+          </div><!-- Left Side -->
+
+          <!-- Right Side -->
+          <div class="col-sm-12 col-md-6 col-lg-6 px-5 pt-sm-2 px-sm-0 d-none d-md-block" id="footer-map">
+            <div id="map" class="w-100 h-100"></div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- Footer Section -->
+
+
+    <!-- Portfolio Modals -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title align-items-center" id="exampleModalLabel">Заголовок проекта</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img src="res/img/backs/dark.png" alt="ALT" class="img-fluid">
+            <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae laudantium quidem sit, quam dicta labore, aperiam. Iusto quae, doloribus quaerat ex facere inventore ipsa sed ipsum ad perferendis commodi eos autem tenetur voluptatibus sequi obcaecati eligendi amet blanditiis laudantium veritatis et eius? Nulla at, natus amet, voluptas inventore molestias quae.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary pointer" data-dismiss="modal">Закрыть</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap js -->
+    <script src="res/jquery/jquery-3.2.1.min.js"></script>
+    <script src="res/popper/popper.js"></script>
+    <script src="res/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- YandexMap js -->
+    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+    <script src="res/js/map.js"></script>
+
+    <!-- Form validation js -->
+    <script src="res/js/validate.js"></script>
+
+    <!-- Menu reaction to scrolling js -->
+    <script src="res/js/scroll.js"></script>
+
+    <!-- Form scrolling js -->
+    <script src="res/js/order.js"></script>
+
+    <!-- Custrom js for form -->
+    <script type="text/javascript">
+      $('input[name="file"]').change(function(){
+        var fileName = $(this).val().replace(/^.*\\/, "");
+        var pathElement = document.getElementById("upload-path");
+        pathElement.innerHTML = fileName;
+      });
+    </script>
+
+  </body>
+</html>
