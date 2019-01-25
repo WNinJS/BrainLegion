10a6ba6edff662b9918c2e60fd39323738946fb3
Max <beardworf@gmail.com>
2019-01-25 19:49:41 +0500

    Update admininfo.php

--- brainlegion.ru/public_html/admininfo.php
+++ brainlegion.ru/public_html/admininfo.php
@@ -18,49 +18,60 @@
         <link rel="stylesheet" href="res/fontello/css/fontello.css">
 
         <!-- Own Styles -->
-        <link rel="stylesheet" href="res/styles.css">
         <link rel="stylesheet" href="cssadmin/adminpanel.css">
     </head>
 <body>
-    <!-- Top Navigation -->
-    <div class="container-fluid px-0 id="top-menu">
-        <nav class="navbar fixed-top-desktop navbar-expand-lg navbar-light" id="top-menu">
-            <!-- Logo -->
-            <a href="adminpanel.php" class="navbar-brand">
-                <img src="res/img/logos/brainlegion_main.svg" alt="FILL" class="img-fluid img-logo">
-            </a>
 
-            <!-- Menu Button for Mobile -->
-            <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <!--Top menu Selection -->
+    <section class="container-fluid sticky-top-desktop px-0" id="top-menu">
 
-            <!-- Menu Itself -->
-            <div class="collapse navbar-collapse text-right" id="navbar-toggler">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a href="admininfo.php" class="nav-link text-purple"><span>Информация</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="adminmain.php" class="nav-link text-purple"><span>Главная</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="adminstudio.php" class="nav-link text-purple"><span>Studio</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="adminfactory.php" class="nav-link text-purple"><span>Factory</span></a>
-                    </li>
-                    <!-- ГАЗИНУР, Логаут и завершение сессии -->
-                    <li class="nav-item">
-                        <a href="adminlogin.php" class="nav-link text-purple"><span>Logout</span></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-    <!-- Top Navigation
-    <script src="res/js/dropMenu.js"></script>
-    -->
+        <!-- Top Navigation -->
+        <div class="container-fluid px-0">
+            <nav class="navbar fixed-top-desktop navbar-expand-lg navbar-light" id="top-menu">
+                
+                <!-- Logo -->
+                <a href="adminpanel.php" class="navbar-brand">
+                    <img src="res/img/logos/brainlegion_main.svg" alt="FILL" class="img-fluid img-logo">
+                </a>
+
+                <!-- Menu Button for Mobile -->
+                <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Menu Itself -->
+                <div class="collapse navbar-collapse text-right" id="navbar-toggler">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="admininfo.php" class="nav-link disabled"><span>Информация</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="adminmain.php" class="nav-link text-purple"><span>Главная</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="adminstudio.php" class="nav-link text-purple"><span>Studio</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="adminfactory.php" class="nav-link text-purple"><span>Factory</span></a>
+                        </li>
+
+                        <!-- ГАЗИНУР, Логаут и завершение сессии -->
+                        <li class="nav-item">
+                            <a href="adminlogin.php" class="nav-link text-purple"><span>Logout</span></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+    </section>
+    
+    <section class="jumbotron jumbotron-fluid jumbotron-light m-0 p-5">
+  <div class="container">
+    <h1 class="display-3">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</section>
 
 
 
@@ -70,3 +81,6 @@
     <script src="res/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
+
+<!--
+    <script src="res/js/dropMenu.js"></script>-->
