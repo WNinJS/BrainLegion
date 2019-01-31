2206e67678d3e0fd4dadf68d82d65e89c6c30615
ishkining <ishkining@mail.ru>
2019-01-31 15:24:26 +0500

    Update deleteImages.js

--- brainlegion.ru/public_html/res/js/deleteImages.js
+++ brainlegion.ru/public_html/res/js/deleteImages.js
@@ -1,12 +1,4 @@
 
 function closeImg(div) {
-	for(var counter = 1; counter < 3; counter++)
-	{
-		var image = "picture" + counter;
-	    if (div.id == image)
-	    {
-	    	alert(div.id);
-	    	document.getElementById(image).style.display = "none";
-		}
-	}	
+	document.getElementById(div.id).style.display = "none";
 }
